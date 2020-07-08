1db3d4154620284d5bd481b3e0044d5ebae010fa
TonyTheCat <anton.shishenya@gmail.com>
2020-07-08 19:30:22 +0300

    PUT rewards

--- routes/rewards.js
+++ routes/rewards.js
@@ -5,6 +5,7 @@ const router = require('express').Router();
 const logger = require('../helpers/logger');
 const { exist, projectOwnerOnly } = require('../middleware/rewards');
 
+const RewardModel = require('../models/reward');
 const RewardChangeLogModel = require('../models/reward_change_log');
 
 router.get('/:id', exist, projectOwnerOnly, async (req, res, next) => {
@@ -22,6 +23,18 @@ router.get('/:id/logs', exist, projectOwnerOnly, async (req, res, next) => {
     const changeLogs = await RewardChangeLogModel.find({ reward_id: reward.id }).exec();
     res.send(changeLogs);
   } catch (err) {
+    logger.error(err);
+    next(new Error(err));
+  }
+});
+
+router.put('/:id', exist, projectOwnerOnly, async (req, res, next) => {
+  try {
+    const { reward } = req;
+    await RewardModel.findByIdAndUpdate(reward.id, req.body);
+    return res.send(await RewardModel.findById(reward.id));
+  } catch (err) {
+    logger.error(err);
     next(new Error(err));
   }
 });
