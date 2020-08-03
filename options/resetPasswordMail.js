27087acec98585e05cf37807260abadbc4343466
adnan-soft-pro <madnankiani1@gmail.com>
2020-08-03 11:19:02 +0300

    Fix template

--- options/resetPasswordMail.js
+++ options/resetPasswordMail.js
@@ -3,8 +3,9 @@ const config = require('../config/index').app;
 exports.resetPassword = (email, token) => ({
   to: email,
   from: config.emailFrom,
-  subject: 'Reset your JustEarlybird password!',
-  html: `<div>Hi there</div>
+  fromname: 'JustEarlybird',
+  subject: 'Reset your JustEarlybird password',
+  html: `<div>Hi there,</div>
   <br/>
   <div>We understand you forgot your password. Don't worry, this happens to the best of us!
    Click this link <a href="${config.frontendURL}/reset/${token}"><strong>here</strong></a>
