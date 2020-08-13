67aab7b2c5aa4b9f4b99414122b3b53efca9de48
TonyTheCat <anton.shishenya@gmail.com>
2020-08-13 11:20:04 +0300

    Fix envs

--- config/index.js
+++ config/index.js
@@ -13,21 +13,21 @@ const getConfig = ()/* : Object */ => ({
     db_url: process.env.DB_URL,
   },
   app: {
-    port: process.env.PORT || 3000,
-    stripeSecret: process.env.STRIPE_SECRET || '',
-    frontendURL: process.env.FRONTEND_URL || 'http://localhost:3000',
+    port: process.env.PORT,
+    stripeSecret: process.env.STRIPE_SECRET,
+    frontendURL: process.env.FRONTEND_URL,
     jwtSecret: process.env.TOKEN_SECRET,
     monitoringUrl: process.env.MONITORING_URL,
     nowPlanPriceId: process.env.NOW_PLAN_PRICE_ID,
-    trialPeriodLaterPlan: process.env.TRIAL_PERIOD_LATER_PLAN || 3,
-    pricePerDayLaterPlan: process.env.PRICE_PER_DAY_LATER_PLAN || 1500,
+    trialPeriodLaterPlan: process.env.TRIAL_PERIOD_LATER_PLAN,
+    pricePerDayLaterPlan: process.env.PRICE_PER_DAY_LATER_PLAN,
     emailFrom: process.env.EMAIL_FROM,
     sendgripApiKey: process.env.SENDGRID_API_KEY,
     trackingId: process.env.TRACKINGID,
   },
   aws: {
-    region: process.env.AWS_REGION || 'us-east-1',
-    dynamoDBName: process.env.DYNAMO_DB || 'test_db',
+    region: process.env.AWS_REGION,
+    dynamoDBName: process.env.DYNAMO_DB,
   },
 
 });
