bc4a00dd14b8d46f1f436522f9d49fffe4918648
adnan-soft-pro <madnankiani1@gmail.com>
2020-09-04 16:13:31 +0300

    Change cod

--- routes/users.js
+++ routes/users.js
@@ -46,7 +46,7 @@ router.post('/setCode', async (req, res, next) => {
   try {
     const user = await UserModel.findById(req.user._id);
     switch (req.body.code) {
-      case 'env3nty5-grow': {
+      case 'enventys20': {
         user.ref_code = req.body.code;
         await user.save();
         user.ref_code = true;
